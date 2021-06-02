8bc8234981b3ba1ca1b54262d3131f802918e63b
MNLaugh <contact@mnlaugh.com>
2021-06-03 01:51:23 +0200

    bad copy/paste on index.js

--- index.js
+++ index.js
@@ -1,144 +1,122 @@
-var express = require('express');
-var router = express.Router();
-var _ = require('lodash');
-var path = require('path');
-var common = require('./common');
 
-// runs on all routes and checks password if one is setup
-router.all('/db/*', common.checkLogin, function (req, res, next){
-    next();
-});
+var fs = require("fs-extra");
+var path = require("path");
+var unzip = require("unzipper");
 
-// create a new database
-router.post('/database/:conn/db_create', function (req, res, next){
-    var connection_list = req.app.locals.dbConnections;
+var mongoClient = require("mongodb").MongoClient;
 
-    // Check for existance of connection
-    if(connection_list[req.params.conn] === undefined){
-        res.status(400).json({'msg': req.i18n.__('Invalid connection')});
-        return;
-    }
+var databaseUri;
+var fileNames = [];
+var client; //global client object
+var db; //global db object
+var zipPath; // path/to/zipfile.zip
+var tempPath = __dirname + "/temp";
 
-    // check for valid DB name
-    if(req.body.db_name.indexOf(' ') >= 0 || req.body.db_name.indexOf('.') >= 0){
-        res.status(400).json({'msg': req.i18n.__('Invalid database name')});
-        return;
-    }
+//var winston = require("winston");
+var ObjectID = require("mongodb").ObjectID;
 
-    // Get DB form pool
-    var mongo_db = connection_list[req.params.conn].native.db(req.body.db_name);
+//this boolean value will determine if the database utilizes the ObjectID class of mongodb
+var isObjectID = false;
 
-    // adding a new collection to create the DB
-    mongo_db.collection('test').save({}, function (err, docs){
-        if(err){
-            console.error('Error creating database: ' + err);
-            res.status(400).json({'msg': req.i18n.__('Error creating database') + ': ' + err});
-        }else{
-            res.status(200).json({'msg': req.i18n.__('Database successfully created')});
-        }
-    });
-});
+async function remove(path) {
+	return await new Promise((resolve, reject) => {
+		fs.remove(path, (error) => (error) ? reject(error) : resolve())
+	})
+}
 
-// delete a database
-router.post('/database/:conn/db_delete', function (req, res, next){
-    var connection_list = req.app.locals.dbConnections;
+async function readJson(path) {
+	return await new Promise((resolve, reject) => fs.readJson(path, (err, data) => (err) ? reject(err) : resolve(data)))
+}
 
-    // Check for existance of connection
-    if(connection_list[req.params.conn] === undefined){
-        res.status(400).json({'msg': req.i18n.__('Invalid connection')});
-    }
+async function restore (dbaseUri, pathToZipFile, useObjectID = false) {
+  if(!dbaseUri || !pathToZipFile) { 
+  	//winston.error("incomplete params \ndbaseUri = " + dbaseUri + "\npathToZipFile = " + pathToZipFile); 
+  	throw new Error("incomplete params \ndbaseUri = " + dbaseUri + "\npathToZipFile = " + pathToZipFile);
+  }
 
-    // Get DB form pool
-    var mongo_db = connection_list[req.params.conn].native.db(req.body.db_name);
+	isObjectID = useObjectID;
+  //winston.error("isObjectID = "  + isObjectID + " useObjectID = " + useObjectID);
 
-    // delete a collection
-    mongo_db.dropDatabase(function (err, result){
-        if(err){
-            console.error('Error deleting database: ' + err);
-            res.status(400).json({'msg': req.i18n.__('Error deleting database') + ': ' + err});
-        }else{
-            res.status(200).json({'msg': req.i18n.__('Database successfully deleted'), 'db_name': req.body.db_name});
-        }
-    });
-});
+	databaseUri = dbaseUri;
+	zipPath = pathToZipFile;
 
-// Backup a database
-router.post('/database/:conn/:db/db_backup', function (req, res, next){
-    //var mongodbBackup = require('mongodb-backup');
-    var Backup = require("backup-mongodb");
-    var MongoURI = require('mongo-uri');
-    var connection_list = req.app.locals.dbConnections;
+	client = await mongoClient.connect(databaseUri, { useUnifiedTopology: true });
+	//winston.info("Restore Script Connected to MongoDb successfully");
+	const dbname = databaseUri.split("/").pop();
+	db = client.db(dbname);
 
-    // Check for existance of connection
-    if(connection_list[req.params.conn] === undefined){
-        res.status(400).json({'msg': req.i18n.__('Invalid connection')});
-    }
+	//winston.info("client && db set");
 
-    // get the URI
-    var conn_uri = MongoURI.parse(connection_list[req.params.conn].connString);
-    var db_name = req.params.db;
+	try {
+		//winston.info("Start extraction...");
+		var unzipExtractor = unzip.Extract({ path: tempPath});
+		fs.createReadStream(zipPath).pipe(unzipExtractor);
+		await new Promise(resolve => unzipExtractor.on("close", resolve));
+		//winston.info("Extraction Complete . . .");
+	
+		var results = await new Promise((resolve, reject) => fs.readdir(tempPath, (err, res) => (err) ? reject(err) : resolve(res)));
+		//winston.info("dir read and contains " + results.length + " files");
+		for(var x in results) {
+			if(results[x].indexOf(".zip") < 0) { // remove the .zip archive
+				fileNames.push(path.win32.basename(results[x], ".json"));
+			}
+			if(x == results.length - 1) { 
+				//winston.info("fileNames = " + fileNames);
+				await loadJsonData(0);
+			}
+		}
+		databaseUri = null;
+		fileNames = [];
+		client = null; //global client object
+		db = null; //global db object
+		zipPath = null; // path/to/zipfile.zip
+	} catch(e) {
+		client.close();
+		throw e;
+	}
+}
 
-    var uri = connection_list[req.params.conn].connString;
+async function loadJsonData(z) {
+	if(z > fileNames.length - 1) { 
+		//winston.info("Restoration procedure complete..."); 
+		if (client && typeof client.close !== "undefined") client.close();
+		await remove(tempPath);
+		//winston.verbose("tempPath removed");
+	}	else {
+		//winston.debug("\nload json data invoked " + z);
+		var collectionName = fileNames[z];
+		//winston.info("collection under processing = " + collectionName + "\n");
+		const data = await readJson(`${tempPath}/${collectionName}.json`);
+		try {
+			await saveToDb(data, 0, collectionName);
+			await loadJsonData(z + 1);
+		} catch(e) {
+			await remove(tempPath);
+			throw e;
+		}
+	}
+}
 
-    // add DB to URI if not present
-    if(!conn_uri.database){
-        uri = uri + '/' + db_name;
-    }
-    console.log(uri, path.join(__dirname, '../backups'))
-    // kick off the backup
-    try {
-        new Backup(uri, path.join(__dirname, '../backups')).backup();
-        res.status(200).json({'msg': req.i18n.__('Database successfully backed up')});
-    } catch(e) {
-        console.error('Backup DB error: ' + e);
-        res.status(400).json({'msg': req.i18n.__('Unable to backup database')});
-    }
-    // mongodbBackup({uri: uri, root: path.join(__dirname, '../backups'), /*tar: db_name + '.tar',*/ callback: function(err){
-        // if(err){
-            // console.error('Backup DB error: ' + err);
-            // res.status(400).json({'msg': req.i18n.__('Unable to backup database')});
-        // }else{
-            // res.status(200).json({'msg': req.i18n.__('Database successfully backed up')});
-        // }
-    // }});
-});
+async function saveToDb(fileData, x, collectionName) {
+	if (x > fileData.length - 1) return; //winston.info("Done Processing " + collectionName + "\n");
+	//winston.verbose("fileData length = " + fileData.length);
+	var collection = fileData[x];
 
-var restore = require("backup-mongodb-restorer");
-// Restore a database
-router.post('/database/:conn/:db/db_restore', async function (req, res, next){
-    var MongoURI = require('mongo-uri');
-    //var mongodbRestore = require('mongodb-restore');
-    
-    var connection_list = req.app.locals.dbConnections;
-    var dropTarget = false;
-    if(req.body.dropTarget === true || req.body.dropTarget === false){
-        dropTarget = req.body.dropTarget;
-    }
+	if(isObjectID) collection._id = ObjectID.createFromHexString(collection._id);
+	if(collection._created_at) collection._created_at = new Date(collection._created_at);
+	if(collection._updated_at) collection._updated_at = new Date(collection._updated_at);	
 
-    // Check for existance of connection
-    if(connection_list[req.params.conn] === undefined){
-        res.status(400).json({'msg': req.i18n.__('Invalid connection')});
-    }
+	// winston.info("collection object = " + collection);
+	const col = db.collection(collectionName);
+	let _id = collection._id //(collection._id.length !== 24) ? collection._id : ObjectID(collection._id);
+	try {
+		const result = await col.replaceOne({ "_id": _id }, collection, { upsert: true });
+	} catch(e) {
+		console.error("Error replaceOne with id: " + _id, e);
+		throw e;
+	}
+	//winston.verbose("update successful " + result);
+	await saveToDb(fileData, (x + 1), collectionName);
+}
 
-    // get the URI
-    var conn_uri = MongoURI.parse(connection_list[req.params.conn].connString);
-    var backup_name = req.params.db
-    var db_name = backup_name.split("_")[0];
-    var uri = connection_list[req.params.conn].connString;
-
-    // add DB to URI if not present
-    if(!conn_uri.database){
-        uri = uri + '/' + db_name;
-    }
-
-    // kick off the restore
-    try {
-        await restore(uri, path.join(__dirname, `../backups/${backup_name}/${backup_name}.zip`));
-        res.status(200).json({'msg': req.i18n.__('Database successfully restored')});
-    } catch(e) {
-        console.error('Restore DB error: ' + e);
-        res.status(400).json({'msg': req.i18n.__('Unable to restore database')});
-    }
-});
-
-module.exports = router;
+module.exports = restore;
